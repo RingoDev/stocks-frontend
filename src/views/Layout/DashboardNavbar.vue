6995cfb7e9dd78bd81125c29e68e8d476f98ba40
ringodev <ringodev@protonmail.com>
2020-10-21 19:24:20 +0200

    removed template scss

--- src/views/Layout/DashboardNavbar.vue
+++ src/views/Layout/DashboardNavbar.vue
@@ -1,84 +1,16 @@
 <template>
   <div class="container-fluid bg-gradient-success">
     <b-navbar class="bg-gradient-success" type="dark" variant="info">
-      <b-navbar-brand href="#">
+      <b-navbar-brand href="/dashboard">
         <img width="64px" src="/img/stocks_512px.png">
       </b-navbar-brand>
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto navbar-dark">
-          <b-nav-item :active="isActiveRoute('Dashboard')" href="/dashboard">Dashboard</b-nav-item>
-
-          <b-nav-item-dropdown>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="/profile">Profile</b-dropdown-item>
-            <b-dropdown-item href="/logout">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav style="color:white" class="ml-auto navbar-dark">
+        <b-nav-item href="/logout">Sign Out</b-nav-item>
+        <b-nav-item :active="isActiveRoute('Profile')" href="/profile">Profile</b-nav-item>
+        <b-nav-item :active="isActiveRoute('Dashboard')" href="/dashboard">Dashboard</b-nav-item>
+      </b-navbar-nav>
     </b-navbar>
-    <!--  <base-nav-->
-    <!--      container-classes="container-fluid"-->
-    <!--      class="navbar-top"-->
-    <!--      :class="{'navbar-dark': type === 'default'}"-->
-    <!--  >-->
-    <!--    <b-navbar-brand class="mr-auto">-->
-    <!--      <img width="40px" src="/img/stocks_512px.png">-->
-    <!--    </b-navbar-brand>-->
-    <!--    &lt;!&ndash; Navbar links &ndash;&gt;-->
-    <!--    <b-navbar-nav class="align-items-center ml-md-auto">-->
-    <!--      <b-nav-item :active="isActiveRoute('Dashboard')" href="/dashboard">Dashboard</b-nav-item>-->
-    <!--      <b-nav-item :active="isActiveRoute('Profile')" href="/profile">Profile</b-nav-item>-->
-    <!--    </b-navbar-nav>-->
-    <!--    <b-navbar-nav class="align-items-center ml-auto ml-md-0">-->
-    <!--      <base-dropdown menu-on-right-->
-    <!--                     class="nav-item"-->
-    <!--                     tag="li"-->
-    <!--                     title-tag="a"-->
-    <!--                     title-classes="nav-link pr-0">-->
-    <!--        <a href="#" class="nav-link pr-0" @click.prevent slot="title-container">-->
-    <!--          <b-media no-body class="align-items-center">-->
-    <!--                  <span class="avatar avatar-sm rounded-circle">-->
-    <!--                    <img alt="Image placeholder" src="img/theme/team-4.jpg">-->
-    <!--                  </span>-->
-    <!--            <b-media-body class="ml-2 d-none d-lg-block">-->
-    <!--              <span class="mb-0 text-sm  font-weight-bold">{{ this.$store.getters.getEmail }}</span>-->
-    <!--            </b-media-body>-->
-    <!--          </b-media>-->
-    <!--        </a>-->
-
-    <!--        <template>-->
-
-    <!--          <b-dropdown-header class="noti-title">-->
-    <!--            <h6 class="text-overflow m-0">Welcome!</h6>-->
-    <!--          </b-dropdown-header>-->
-    <!--          <b-dropdown-item href="#!">-->
-    <!--            <i class="ni ni-single-02"></i>-->
-    <!--            <span>My profile</span>-->
-    <!--          </b-dropdown-item>-->
-    <!--          <b-dropdown-item href="#!">-->
-    <!--            <i class="ni ni-settings-gear-65"></i>-->
-    <!--            <span>Settings</span>-->
-    <!--          </b-dropdown-item>-->
-    <!--          <b-dropdown-item href="#!">-->
-    <!--            <i class="ni ni-calendar-grid-58"></i>-->
-    <!--            <span>Activity</span>-->
-    <!--          </b-dropdown-item>-->
-    <!--          <b-dropdown-item href="#!">-->
-    <!--            <i class="ni ni-support-16"></i>-->
-    <!--            <span>Support</span>-->
-    <!--          </b-dropdown-item>-->
-    <!--          <div class="dropdown-divider"></div>-->
-    <!--          <b-dropdown-item href="/logout">-->
-    <!--            <i class="ni ni-user-run"></i>-->
-    <!--            <span>Logout</span>-->
-    <!--          </b-dropdown-item>-->
-
-    <!--        </template>-->
-    <!--      </base-dropdown>-->
-    <!--    </b-navbar-nav>-->
-    <!--  </base-nav>-->
   </div>
 </template>
 <script>
